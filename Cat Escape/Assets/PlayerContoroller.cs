a742e1cc96264609d943dcd2f0bb7fffaa10470b
honoka <g1727006@st.t-kougei.ac.jp>
2018-06-04 18:15:14 +0900

    当たり判定

---
+++ Cat Escape/Assets/PlayerContoroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoroller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //左矢印が押された時
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0);//左に「３」動かす
+        }
+        //右矢印が押された時
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0);
+        }
+	}
+}
